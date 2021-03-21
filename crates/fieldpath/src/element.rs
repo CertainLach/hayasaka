647be3268782bfac81729ee0f831a00b277b43a8
Yaroslav Bolyukin <iam@lach.pw>
2021-03-21 17:17:58 +0300

    feat: escape fieldpath

--- crates/fieldpath/src/element.rs
+++ crates/fieldpath/src/element.rs
@@ -8,11 +8,20 @@ pub enum Element {
     Select(String, Value),
     Index(usize),
 }
+
+fn write_field(f: &mut fmt::Formatter<'_>, n: &str) -> fmt::Result {
+    if n.contains(|c| c == '"' || c == '.') {
+        write!(f, ".\"{}\"", n.replace("\"", "\\\""))
+    } else {
+        write!(f, ".{}", n)
+    }
+}
+
 impl Display for Element {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::StaticField(field) => write!(f, ".{}", field),
-            Self::Field(field) => write!(f, ".{}", field),
+            Self::StaticField(field) => write_field(f, field),
+            Self::Field(field) => write_field(f, field),
             Self::Select(key, value) => write!(f, "[{}={}]", key, value),
             Self::Index(idx) => write!(f, "{}", idx),
         }
