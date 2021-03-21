4febfc4037e14985bec83b1880759dae2a3d11ae
Yaroslav Bolyukin <iam@lach.pw>
2021-03-21 17:19:56 +0300

    feat: parse escaped fieldpath

--- crates/fieldpath/src/parse.rs
+++ crates/fieldpath/src/parse.rs
@@ -4,17 +4,27 @@ use serde_json::Value;
 
 peg::parser! {
     pub grammar parser() for str {
-        rule field() -> Element
-            = "." name:$((!['.' | '\n' | '['][_])+) {
+        rule string() -> String
+            = "\"" str:$((("\\\"")+ / (!['"' | '\\'][_])+)+) "\"" {
+                str.replace("\\\"", "\"")
+            }
+            / "'" str:$((("\\'")+ / (!['\''][_])+)+) "'" {
+                str.replace("\\'", "'")
+            }
+       rule field() -> Element
+            = "." name:string() {
+                Element::Field(name)
+            }
+            / "." name:$((!['.' | '\n' | '['][_])+) {
                 Element::Field(name.to_owned())
-            };
+            }
         rule index() -> Element
             = "[" idx:$(['0'..='9']+) "]" {
                 Element::Index(idx.parse().unwrap())
             }
         rule selector() -> Element
-            = "[" key:$((!['='][_])+) "=\"" value:$((!['"'][_])+) "\"]" {
-                Element::Select(key.to_owned(), Value::String(value.to_owned()))
+            = "[" key:$((!['='][_])+) "=\"" value:string() "]" {
+                Element::Select(key.to_owned(), Value::String(value))
             }
         rule element() -> Element
             = field()
@@ -28,4 +38,23 @@ peg::parser! {
 
 pub fn parse(input: &str) -> Result<PathBuf, peg::error::ParseError<LineCol>> {
     parser::path(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse;
+    use crate::{Element::*, PathBuf};
+
+    #[test]
+    fn escaping() {
+        assert_eq!(
+            parse(r#".aaa."aaa"."aa.aa"."aa\"aa""#).unwrap(),
+            PathBuf(vec![
+                Field("aaa".to_owned()),
+                Field("aaa".to_owned()),
+                Field("aa.aa".to_owned()),
+                Field("aa\"aa".to_owned()),
+            ])
+        )
+    }
 }
