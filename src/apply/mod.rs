ed3035a4cc497c8e7b00c2037bbd262e412b9a5a
Yaroslav Bolyukin <iam@lach.pw>
2021-03-25 17:25:43 +0300

    fix: do not prune EndpointSlice
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- src/apply/mod.rs
+++ src/apply/mod.rs
@@ -283,6 +283,8 @@ pub async fn apply_multi(
             // Endpoints copies Service labels
             if item.kind.api_version == "v1" && item.kind.kind == "Endpoints" {
                 continue;
+            } else if item.kind.api_version == "discovery.k8s.io/v1beta1" && item.kind.kind == "EndpointSlice" {
+                continue;
             }
 
             log::warn!("pruning {}", item);
